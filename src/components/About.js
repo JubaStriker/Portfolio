e55067d4b7781d3578672cb8e54163eada37b104
JubaStriker <jubastriker@gmail.com>
2022-12-11 14:18:08 +0600

    scroll animation added

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import AOS from 'aos';
 
 const About = () => {
     return (
         <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
             <div className='flex flex-col justify-center items-center w-full h-full'>
                 <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
-                    <div className='sm:text-right pb-8 pl-4'>
+                    <div data-aos="fade-right" className='sm:text-right pb-8 pl-4'>
                         <p className='text-4xl font-bold inline border-b-4 border-pink-700'>About</p>
                     </div>
                     <div>
@@ -13,10 +14,10 @@ const About = () => {
                     </div>
                 </div>
                 <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
-                    <div className='sm:text-right text-4xl font-bold'>
+                    <div data-aos="fade-right" className='sm:text-right text-4xl font-bold'>
                         <p>Hi, I'm Jubair, nice to meet you. Please take a look around</p>
                     </div>
-                    <div>
+                    <div data-aos="fade-left">
                         <p>
                             I am passionate about building excellent software that improves
                             the lives of those around me. I specialize in creating software
