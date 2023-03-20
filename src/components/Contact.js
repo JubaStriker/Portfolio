6239225e855cefb6502d4668edef7d32d1ad09f2
JubaStriker <jubastriker@gmail.com>
2023-03-21 02:52:06 +0600

    padding fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
                         <p className='text-4xl text-gray-300 font-bold inline border-b-4 border-pink-600'>Contact me</p>
                         <p className='text-gray-300 py-4'>Send me an email - jubairhossain0604@gmail.com</p>
                     </div>
-                    <input className='my-4 p-1 bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
+                    <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
                     <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
                     <textarea className='my-4 p-2 bg-[#ccd6f6]' name="message" id="" rows="10" placeholder='Type your message here'></textarea>
                     <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
